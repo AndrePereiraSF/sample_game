7f057d5a3c0a0135f6f85c7102d5df391b9a7ff6
AndrePereiraSF <andre-filho08@hotmail.com>
2019-04-09 15:33:44 -0300

    Fix some bugs

--- Player.gd
+++ Player.gd
@@ -1,19 +1,18 @@
 extends KinematicBody2D
 
 const GRAVITY = 200.0
-const WALK_SPEED = 2
+const WALK_SPEED = 200
 
-func _ready():
-	pass 
+var velocity = Vector2()
 
 func _physics_process(delta):
-	var velocity = Vector2()
-	velocity.y += delta * GRAVITY
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = WALK_SPEED
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-	else:
-		velocity.x = 0
-	var motion = velocity * GRAVITY
-	move_and_slide(motion, Vector2(0, -1))
+    velocity.y += delta * GRAVITY
+
+    if Input.is_action_pressed("ui_left"):
+        velocity.x = -WALK_SPEED
+    elif Input.is_action_pressed("ui_right"):
+        velocity.x =  WALK_SPEED
+    else:
+        velocity.x = 0
+
+    move_and_slide(velocity, Vector2(0, -1))
