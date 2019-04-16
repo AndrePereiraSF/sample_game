78102b0f18550169438c52179483194905cd1574
AndrePereiraSF <andre-filho08@hotmail.com>
2019-04-16 14:40:32 -0300

    Change Player.gd

--- Player.gd
+++ Player.gd
@@ -6,21 +6,42 @@ const SPEED = 300
 const JUMP_HEIGHT = -550
 var motion = Vector2()
 var double = false
+var screensize
 
 func jump():
 	motion.y = JUMP_HEIGHT
+
+func walk(direction):
+	if direction == "left":
+		motion.x = -SPEED
+		$AnimatedSprite.animation = "Walk"
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play()
+	elif direction == "right":
+		$AnimatedSprite.animation = "Walk"
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play()
+		motion.x = SPEED
+		$CollisionShape2D.position
+
+func stop():
+	motion.x = 0
+	$AnimatedSprite.animation = "Idle"
+	$AnimatedSprite.play()
+
+func _ready():
+	screensize = get_viewport_rect().size
 
 func _physics_process(delta):
 	
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		walk("right")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		walk("left")
 	else:
-		motion.x = 0
-		
+		stop()
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			jump()
@@ -30,5 +51,8 @@ func _physics_process(delta):
 		if Input.is_action_pressed("ui_up"):
 			jump()
 			double = false
-	
+
+	if $CollisionShape2D.position.x >= screensize.x - 1000:
+		stop()
+		
 	motion = move_and_slide(motion, UP)
