cf6f01c5db6bb6af1f26e37b051f364cefefdb44
AndrePereiraSF <andre-filho08@hotmail.com>
2019-04-16 19:39:55 -0300

    Add double jump and screen limit

--- Player.gd
+++ Player.gd
@@ -2,22 +2,22 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 300
-const JUMP_HEIGHT = -550
+export var jump_heigth = -350
+export var speed = 200
 var motion = Vector2()
-var double = false
+var can_double_jump = false
 var screensize
 
 func jump():
-	motion.y = JUMP_HEIGHT
+	motion.y = jump_heigth
 
 func walk(direction):
 	if direction == "left":
-		motion.x = -SPEED
+		motion.x = -speed
 		$Sprite/AnimationPlayer.play("Walk")
 		$Sprite.flip_h = true
 	elif direction == "right":
-		motion.x = SPEED
+		motion.x = speed
 		$Sprite/AnimationPlayer.play("Walk")
 		$Sprite.flip_h = false
 
@@ -30,17 +30,21 @@ func _ready():
 	screensize = get_viewport_rect().size
 
 func _physics_process(delta):
-	
+	position.x = clamp(position.x, 0, screensize.x )
 	motion.y += GRAVITY
-	
+
 	if Input.is_action_pressed("ui_right"):
 		walk("right")
 	elif Input.is_action_pressed("ui_left"):
 		walk("left")
 	else:
 		stop()
-	if is_on_floor():
-		if Input.is_action_pressed("ui_select"):
-			jump()
+
+	if Input.is_action_just_pressed("ui_select") and can_double_jump:
+		jump()
+		can_double_jump = false
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+		jump()
+		can_double_jump = true
 
 	motion = move_and_slide(motion, UP)
